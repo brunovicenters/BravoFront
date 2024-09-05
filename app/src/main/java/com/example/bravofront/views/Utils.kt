e886632adca680d3125ae127d7f9be67c3ac1420
bruno <brunovicente.dev@gmail.com>
2024-09-05 10:57:12 -0300

    Added price formatting

--- app/src/main/java/com/example/bravofront/views/Utils.kt
+++ app/src/main/java/com/example/bravofront/views/Utils.kt
@@ -2,6 +2,8 @@ package com.example.bravofront.views
 
 import android.view.View
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import java.text.NumberFormat
+import java.util.*
 
 fun turnOnLoading(swiper: SwipeRefreshLayout, progressBar: View, container: View) {
     swiper.isRefreshing = true
@@ -12,4 +14,14 @@ fun turnOffLoading(swiper: SwipeRefreshLayout, progressBar: View, container: Vie
     swiper.isRefreshing = false
     progressBar.visibility = View.GONE
     container.visibility = View.VISIBLE
+}
+
+fun formatPrice(price: Double) : String {
+    val format = NumberFormat.getCurrencyInstance()
+    format.maximumFractionDigits = 2
+
+    // TODO: change according to idiom
+    format.currency = Currency.getInstance("BRL")
+
+    return format.format(price)
 }
