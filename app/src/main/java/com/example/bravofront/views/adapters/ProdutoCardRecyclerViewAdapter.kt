258175e115c45efbd1e2083063dc567563fb490e
bruno <brunovicente.dev@gmail.com>
2024-09-05 10:56:33 -0300

    Added formatting

--- app/src/main/java/com/example/bravofront/views/adapters/ProdutoCardRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/bravofront/views/adapters/ProdutoCardRecyclerViewAdapter.kt
@@ -1,29 +1,38 @@
-package com.example.bravofront.views
+package com.example.bravofront.views.adapters
 
 import android.graphics.Paint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bravofront.R
 import com.example.bravofront.databinding.CardItemBinding
 import com.example.bravofront.model.ProdutoIndex
+import com.example.bravofront.views.formatPrice
 import com.squareup.picasso.Picasso
-import java.util.*
 
 class ProdutoCardRecyclerViewAdapter(private val list: List<ProdutoIndex>)
     : RecyclerView.Adapter<ProdutoCardRecyclerViewAdapter.ViewHolder>() {
 
+
+
     class ViewHolder (private val binding: CardItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(produto: ProdutoIndex) {
-            binding.cardTitle.text = produto.nome
+            if (produto.nome?.length!! > 10) {
+                val newText = produto.nome.take(10) + "..."
+                binding.cardTitle.text = newText
+            } else {
+                binding.cardTitle.text = produto.nome
+            }
 
             if(produto.desconto != null && produto.desconto.toDouble() > 0.0) {
-                val newPrice = (produto.preco?.toDouble() ?: 0.0) - (produto.desconto.toDouble() ?: 0.0)
-                binding.cardPrice.text = String.format(Locale.getDefault(), "%s", newPrice)
-                binding.cardOldPrice.text = String.format(Locale.getDefault(), "%s", produto.preco)
+                val newPrice = (produto.preco.toDouble() ?: 0.0) - (produto.desconto.toDouble() ?: 0.0)
+                binding.cardPrice.text = formatPrice(newPrice)
+                binding.cardOldPrice.text = formatPrice(produto.preco.toDouble())
                 binding.cardOldPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
             } else {
-                binding.cardPrice.text = String.format(Locale.getDefault(), "%s", produto.preco)
+                binding.cardPrice.text = formatPrice(produto.preco.toDouble())
+                binding.cardOldPrice.visibility = View.GONE
             }
 
             Picasso.get()
