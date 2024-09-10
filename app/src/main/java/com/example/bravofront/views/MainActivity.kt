af7f5a162233b45ae51aa28bd6acdd8f05f09e4a
bruno <brunovicente.dev@gmail.com>
2024-09-10 18:10:40 -0300

    Changed fragment selection to onResume

--- app/src/main/java/com/example/bravofront/views/MainActivity.kt
+++ app/src/main/java/com/example/bravofront/views/MainActivity.kt
@@ -1,48 +1,36 @@
 package com.example.bravofront.views
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.bravofront.R
 import com.example.bravofront.databinding.ActivityMainBinding
 import com.example.bravofront.views.fragments.HomeFragment
+import com.example.bravofront.views.fragments.LoggedProfileFragment
 import com.example.bravofront.views.fragments.SearchFragment
 import com.example.bravofront.views.fragments.UnLoggedProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding;
+    lateinit var binding : ActivityMainBinding
 
-    lateinit var frag : Fragment;
+    lateinit var frag : Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        when (intent.getIntExtra("screen", 0)) {
-//            R.id.search -> {
-//                frag = SearchFragment.newInstance("", "")
-//                supportFragmentManager
-//                    .beginTransaction()
-//                    .replace(binding.fragContainer.id, frag)
-//                    .commit()
-//            }
-//            R.id.profile -> {
-//                frag = UnLoggedProfileFragment.newInstance(this)
-//                supportFragmentManager
-//                    .beginTransaction()
-//                    .replace(binding.fragContainer.id, frag)
-//                    .commit()
-//            }
-//            else -> {
-                frag = HomeFragment.newInstance()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(binding.fragContainer.id, frag)
-                    .commit()
-//            }
-//        }
+        frag = HomeFragment.newInstance()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.fragContainer.id, frag)
+            .commit()
+    }
+
+    override fun onResume() {
+        super.onResume()
 
         binding.bottomNavView.setOnItemSelectedListener {
             when (it.itemId) {
@@ -61,11 +49,20 @@ class MainActivity : AppCompatActivity() {
                         .commit()
                 }
                 R.id.profile -> {
-                    frag = UnLoggedProfileFragment.newInstance(this)
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(binding.fragContainer.id, frag)
-                        .commit()
+                    if (false) {
+                        frag = LoggedProfileFragment.newInstance()
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.fragContainer.id, frag)
+                            .commit()
+                    } else {
+                        frag = UnLoggedProfileFragment.newInstance(this)
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(binding.fragContainer.id, frag)
+                            .commit()
+                    }
+
                 }
             }
             true
