b86a33cf77dc0ef391ea96c06d141d62fa4449ea
bruno <brunovicente.dev@gmail.com>
2024-09-10 17:30:05 -0300

    Deleted cookie catcher

--- app/src/main/java/com/example/bravofront/api/API.kt
+++ app/src/main/java/com/example/bravofront/api/API.kt
@@ -1,7 +1,5 @@
 package com.example.bravofront.api
 
-import okhttp3.Cookie
-import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,16 +7,13 @@ import java.util.concurrent.TimeUnit
 
 object API {
 
-    private const val BASE_URL = "http://10.135.151.17:8000/api/"
+    private const val BASE_URL = "http://10.135.151.17:8000/"
 
     private const val TIMEOUT = 30L
-
-    private val cookieJar = APICookieJar()
 
     private val retrofit: Retrofit
         get() {
             val okHttp = OkHttpClient.Builder()
-                .cookieJar(cookieJar)
                 .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
                 .readTimeout(TIMEOUT, TimeUnit.SECONDS)
                 .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
@@ -40,9 +35,4 @@ object API {
 
     val login: LoginAPI
         get() = retrofit.create(LoginAPI::class.java)
-
-    fun getCookiesForUrl(url: String): List<Cookie> {
-        val httpUrl = url.toHttpUrlOrNull()
-        return httpUrl?.let { cookieJar.getCookies(it) } ?: ArrayList()
-    }
 }
