3b9dd48c915848325dd4bd806d3786fca3c1f6f1
bruno <brunovicente.dev@gmail.com>
2024-09-10 23:13:36 -0300

    Added basic template to retrofit call

--- app/src/main/java/com/example/bravofront/views/LoginActivity.kt
+++ app/src/main/java/com/example/bravofront/views/LoginActivity.kt
@@ -3,7 +3,14 @@ package com.example.bravofront.views
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.bravofront.api.API
 import com.example.bravofront.databinding.ActivityLoginBinding
+import com.example.bravofront.model.ApiResponse
+import com.example.bravofront.model.Login
+import com.google.android.material.snackbar.Snackbar
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
@@ -16,6 +23,33 @@ class LoginActivity : AppCompatActivity() {
 
 
         binding.btnEntrar.setOnClickListener {
+
+            val email = binding.inputEmail
+            val password = binding.inputPassword
+
+            if (email.text.toString().isEmpty() || password.text.toString().isEmpty()) {
+                makeToast("Preencha todos os campos!", this)
+                return@setOnClickListener
+            }
+
+            if (email.error != null || password.error != null) {
+                makeToast("E-mail ou senha inválidos!", this)
+                return@setOnClickListener
+            }
+
+            val callback = object : Callback<ApiResponse<Login>> {
+                override fun onResponse(p0: Call<ApiResponse<Login>>, p1: Response<ApiResponse<Login>>) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onFailure(p0: Call<ApiResponse<Login>>, p1: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+            }
+
+            API(null).login.login(email.text.toString(), password.text.toString()).enqueue(callback)
+
             finish()
         }
     }
