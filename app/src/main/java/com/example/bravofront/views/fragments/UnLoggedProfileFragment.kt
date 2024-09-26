6cc58131c4c166fb926485cdad72795fb509fccc
bruno <brunovicente.dev@gmail.com>
2024-09-26 20:00:23 -0300

    Deleted comments

--- app/src/main/java/com/example/bravofront/views/fragments/UnLoggedProfileFragment.kt
+++ app/src/main/java/com/example/bravofront/views/fragments/UnLoggedProfileFragment.kt
@@ -15,31 +15,9 @@ import com.example.bravofront.views.RegisterActivity
 import com.example.bravofront.views.TermAndConditionsActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//private const val ARG_PARAM1 = "param1"
-//private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UnLoggedProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UnLoggedProfileFragment(val ctx: Context) : Fragment() {
 
     lateinit var binding: FragmentUnLoggedProfileBinding
-
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -74,22 +52,8 @@ class UnLoggedProfileFragment(val ctx: Context) : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(ctx: Context) =
             UnLoggedProfileFragment(ctx)
-//                .apply {
-//                    arguments = Bundle().apply {
-//                        putString(ARG_PARAM1, ctx)
-//                    }
-//                }
     }
 }
